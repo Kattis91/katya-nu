0894141c8cde536224c53fa343c053c4ff2395d9
Kattis91 <katya.svedmark@gmail.com>
2025-11-05 18:43:00 +0100

    Create a separate translations file

---
+++ js/translations.js
@@ -0,0 +1,16 @@
+const translations = {
+    en: {
+        // Navigation
+        home: "Home",
+        about: "About",
+        projects: "Projects",
+        contact: "Contact",
+    },
+    sv: {
+        // Navigation
+        home: "Hem",
+        about: "Om mig",
+        projects: "Projekt",
+        contact: "Kontakt",
+    }
+};
